cbbedb7abfac348b014b8738c4aa0b0040a00cb2
JBradachi <joao.bradachi@ufv.br>
2025-01-20 22:10:49 -0300

    feat: conectando bd 2

--- Site/backend/app.py
+++ Site/backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, render_template, url_for, request
 from markupsafe import escape
 import requests
+import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 
@@ -20,9 +22,27 @@ with app.test_request_context():
 @app.route('/noticias', methods=['GET', 'POST'])
 def noticias():
     if request.method == 'POST':
-        return "Executa POST, envia dados para serem processados"
-    else:
-        return render_template("noticias.html")
+        
+        # pega request do site
+        noticia = request.form.get("noticia")
+
+    
+        # TODO inserir no db a notícia
+        mydb = mysql.connector.connect(
+            user="root",
+            password="root",
+            database="mysqlsite",
+            port="3306"
+        )
+        cursor = mydb.cursor()
+        insere_noticia = ("INSERT INTO TB_noticia "
+                          "(titulo) VALUES (%s)")
+        dados_noticia = {noticia}
+        cursor.execute(insere_noticia, dados_noticia)
+
+    # TODO conectar com db e gerar as consultas
+    noticias = []
+    return render_template("noticias.html", noticias=noticias)
 
 # é possível separar usando marcadores
 
